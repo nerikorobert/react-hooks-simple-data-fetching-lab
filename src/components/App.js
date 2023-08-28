a677b5494561df31c7de80a1bd57df53d2e64596
robert neriko <nerikorobert@gmail.com>
2023-08-28 11:03:38 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,33 @@
+
+import { useEffect, useState } from "react"
+
 // create your App component here
+function App(){
+    const[image , setImage]=useState(null)
+    const fetchData=async ()=>{
+        const response = await fetch("https://dog.ceo/api/breeds/image/random")
+        if(response.status !== 200){
+            throw new Error("image not found")
+        }
+        const data =  await response.json()
+       return data
+    }
+
+    useEffect(()=>{
+        fetchData()
+        .then(data=>{
+            setImage(data.message)
+        })
+        .catch(e=>console.log(e))
+    }, [])
+ 
+    // console.log(image)
+    return (
+        <div>
+            {image === null ? <p>Loading</p> : <img src={image} alt="A Random Dog"></img>}
+        </div>
+    )
+
+}
+
+export default App;
